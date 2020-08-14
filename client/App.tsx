bd507b99463d14230909aaa3d012113c35bb4b36
Maylynn <maylynn.ng94@gmail.com>
2020-08-14 18:58:15 +0100

    feat: install react-native-maps

--- client/App.tsx
+++ client/App.tsx
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
+
+type Props = {
+  latitude: number;
+  longitude: number;
+  latitudeDelta: number;
+  longitudeDelta: number;
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView />
     </View>
   );
 }
