871723404345946ee4483f05f037aa63e7a4127e
Ishdril <berni2015@gmail.com>
2020-08-15 14:24:08 +0200

    feat(map): map displays with poligons of all the boroughs. Boroughs can be tapped and log their name

--- client/App.tsx
+++ client/App.tsx
@@ -1,8 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import MapView, { Geojson } from 'react-native-maps';
-import boroughsJSON from './assets/london_boroughs.json';
+import MapView, { Geojson, Polygon } from 'react-native-maps';
+import boroughsJSON from './assets/london_sport.json';
 
 type LatLng = {
   latitude: Number;
@@ -11,22 +11,47 @@ type LatLng = {
 
 const boroughs = JSON.parse(JSON.stringify(boroughsJSON));
 
+const handlePress = (name: String) => {
+  console.info(name);
+};
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hi!!!</Text>
       <MapView
         initialRegion={{
-          latitude: 51.3290110106029,
-          longitude: -0.330679062942453,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
+          latitude: 51.509993,
+          longitude: -0.104298,
+          latitudeDelta: 0.7,
+          longitudeDelta: 0.7,
         }}
         style={{ width: '100%', height: 500 }}
       >
-        <Geojson geojson={boroughs} strokeColor="red" fillColor="green" strokeWidth={2} />
+        {boroughs.features.map(borough => {
+          return (
+            <Polygon
+              key={borough.properties.id}
+              coordinates={borough.geometry.coordinates[0].map(coords => {
+                console.log(coords);
+                return {
+                  latitude: coords[1],
+                  longitude: coords[0],
+                };
+              })}
+              strokeWidth={1}
+              fillColor="rgba(0, 250, 70, 0.6)"
+              tappable={true}
+              onPress={() => handlePress(borough.properties.name)}
+            />
+          );
+        })}
+        {/* <Geojson
+          geojson={boroughs}
+          strokeColor="red"
+          fillColor="rgba(0, 250, 70, 0.6)"
+          strokeWidth={2}
+        /> */}
       </MapView>
-      <Text>Hello!</Text>
     </View>
   );
 }
