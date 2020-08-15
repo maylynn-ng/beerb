27c6889f70adc16cf8c7ea2b59f50e4082f16448
Ishdril <berni2015@gmail.com>
2020-08-15 18:44:33 +0200

    refactor(map): place map in a separate screen and created models for 'borough' & 'Coordinates'

--- client/App.tsx
+++ client/App.tsx
@@ -1,58 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import MapView, { Geojson, Polygon } from 'react-native-maps';
-import boroughsJSON from './assets/london_sport.json';
+import { StyleSheet, Platform, StatusBar as StatBar, SafeAreaView } from 'react-native';
 
-type LatLng = {
-  latitude: Number;
-  longitude: Number;
-};
-
-const boroughs = JSON.parse(JSON.stringify(boroughsJSON));
-
-const handlePress = (name: String) => {
-  console.info(name);
-};
+import boroughs from './assets/london_sport.json';
+import MapScreen from './Screens/MapScreen';
 
 export default function App() {
+  const handlePress = (name: String): void => {
+    console.info(name);
+  };
+
   return (
-    <View style={styles.container}>
-      <MapView
-        initialRegion={{
-          latitude: 51.509993,
-          longitude: -0.104298,
-          latitudeDelta: 0.7,
-          longitudeDelta: 0.7,
-        }}
-        style={{ width: '100%', height: 500 }}
-      >
-        {boroughs.features.map(borough => {
-          return (
-            <Polygon
-              key={borough.properties.id}
-              coordinates={borough.geometry.coordinates[0].map(coords => {
-                console.log(coords);
-                return {
-                  latitude: coords[1],
-                  longitude: coords[0],
-                };
-              })}
-              strokeWidth={1}
-              fillColor="rgba(0, 250, 70, 0.6)"
-              tappable={true}
-              onPress={() => handlePress(borough.properties.name)}
-            />
-          );
-        })}
-        {/* <Geojson
-          geojson={boroughs}
-          strokeColor="red"
-          fillColor="rgba(0, 250, 70, 0.6)"
-          strokeWidth={2}
-        /> */}
-      </MapView>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <MapScreen handlePress={handlePress} boroughs={boroughs} />
+      <StatusBar style="auto" />
+    </SafeAreaView>
   );
 }
 
@@ -62,5 +24,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? StatBar.currentHeight : 0,
   },
 });
