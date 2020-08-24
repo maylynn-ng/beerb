3b652e5291ded4084cd68062ed79aab422d3ec99
Ishdril <berni2015@gmail.com>
2020-08-24 12:02:53 +0200

    fix(beerdex): loading now renders loading screen

--- Screens/Beerdex.tsx
+++ Screens/Beerdex.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { Text, View, StyleSheet, SafeAreaView } from 'react-native';
-import { getBeerdex, getDrunkBeers } from '../redux/actions';
+import { getBeerdex, getDrunkBeers, changeLoading } from '../redux/actions';
 import { connect } from 'react-redux';
 import { StatusBar } from 'expo-status-bar';
 
@@ -10,12 +10,19 @@ import { Beer } from '../Models/Beer.model';
 import BeerBadge from '../Components/BeerBadge';
 import { ScrollView } from 'react-native-gesture-handler';
 
-function Beerdex({ user, populateBeerdex, populateDrunkBeers, beerdex }: any) {
+function Beerdex({ user, populateBeerdex, populateDrunkBeers, beerdex, setLoading }: any) {
   useEffect(() => {
+    setLoading(true);
     populateBeerdex();
 
     populateDrunkBeers(orderDrunkBeers());
   }, []);
+
+  useEffect(() => {
+    if (beerdex && beerdex.length) {
+      setLoading(false);
+    }
+  }, [beerdex]);
 
   function orderDrunkBeers() {
     return Array.from(new Set(user.Locations.map((entry: any) => entry.beerId)));
@@ -36,16 +43,15 @@ function Beerdex({ user, populateBeerdex, populateDrunkBeers, beerdex }: any) {
                     )
                 )
               : null}
-            {beerdex && beerdex.length ? (
-              beerdex.map((beer: Beer, index: number) => {
-                if (orderDrunkBeers().indexOf(beer.beerId) === -1) {
-                  return <BeerBadge style={styles.badge} hasDrunk={0.3} key={index} beer={beer} />;
-                }
-              })
-            ) : (
-              // <Loading />
-              <Text>Loading</Text>
-            )}
+            {beerdex && beerdex.length
+              ? beerdex.map((beer: Beer, index: number) => {
+                  if (orderDrunkBeers().indexOf(beer.beerId) === -1) {
+                    return (
+                      <BeerBadge style={styles.badge} hasDrunk={0.3} key={index} beer={beer} />
+                    );
+                  }
+                })
+              : null}
           </View>
         </ScrollView>
       </View>
@@ -64,6 +70,7 @@ function mapDispatch(dispatch: any) {
   return {
     populateBeerdex: () => dispatch(getBeerdex()),
     populateDrunkBeers: (beerIds: []) => dispatch(getDrunkBeers(beerIds)),
+    setLoading: (status: boolean) => dispatch(changeLoading(status)),
   };
 }
 
