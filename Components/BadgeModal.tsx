7bc9fdd3d34185809cda1b659bef2b6aab643746
Ishdril <berni2015@gmail.com>
2020-08-26 17:09:06 +0200

    feat: small changes

--- Components/BadgeModal.tsx
+++ Components/BadgeModal.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Image, Text, View, StyleSheet } from 'react-native';
 import { Badge } from '../Models/Badge.model';
 
-function BadgeModal({ badge }) {
+function BadgeModal({ badge }: any) {
   return (
     <View style={styles.screen}>
       <View style={styles.container}>
